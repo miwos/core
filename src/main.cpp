97b5300a0ceec2176c5efed8aa6a0dd077e56e8a
arnoson <omasocke@hotmail.de>
2022-09-10 16:59:44 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <Bridge.h>
+#include <FileSystem.h>
+#include <Lua.h>
+#include <LuaFileSystem.h>
+#include <LuaLog.h>
+#include <LuaTimer.h>
+#include <SlipSerial.h>
+
+SlipSerial serial(Serial);
+
+int ref;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+
+  Bridge::begin(serial);
+  FileSystem::begin();
+  Lua::begin();
+
+  Lua::onSetup([]() {
+    LuaLog::install();
+    LuaFileSystem::install();
+    LuaTimer::install();
+  });
+}
+
+void loop() {
+  Bridge::update();
+  LuaTimer::update();
+}
