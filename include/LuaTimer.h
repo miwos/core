af8bec4090d482ab931a70c3ffec45a07182fffa
arnoson <omasocke@hotmail.de>
2022-10-22 16:23:51 +0200

    refactor: simplify lua function ref

--- include/LuaTimer.h
+++ include/LuaTimer.h
@@ -9,12 +9,6 @@ namespace LuaTimer {
 
   namespace {
     int updateRef = -1;
-    bool updateIsStored = false;
-
-    void storeUpdate() {
-      updateRef = Lua::storeFunction("Timer", "update");
-      if (updateRef != -1) updateIsStored = true;
-    }
   } // namespace
 
   int now(lua_State *L) {
@@ -23,20 +17,19 @@ namespace LuaTimer {
   }
 
   void install() {
-    updateIsStored = false;
-    storeUpdate();
+    updateRef = -1;
     luaL_Reg lib[] = {{"now", now}, {NULL, NULL}};
     luaL_register(Lua::L, "Timer", lib);
   }
 
   void update() {
-    // `__update__` will only be available after `init.lua` has been executed
-    // so we might have to try multiple times.
-    if (!updateIsStored) storeUpdate();
-
     currentTime = micros();
+
     // Don't log an error if we can't find the function because this gets called
     // thousands of times per second!
+    if (updateRef == -1)
+      updateRef = Lua::storeFunction("Timer", "update", false);
+
     if (Lua::getFunction(updateRef, false)) {
       lua_pushinteger(Lua::L, currentTime);
       lua_pcall(Lua::L, 1, 0, 0);
